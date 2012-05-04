f4c2298fd8afacdf8e4544e890ea5da183defa44
Justin Carmony <justin@justincarmony.com>
2012-05-04 03:58:09 -0600

    Working on web ui

--- master/www/index.php
+++ master/www/index.php
@@ -316,7 +316,7 @@
 				}
 
 				var server_html = '<div id="' + server_k + '" class="well server_listing" style="width:200px;"><p class="info">'
-					+ '<strong>IP:</strong> '+ server_data.ip + '<br /> <strong>Hostname: </strong></p></div>'
+					+ '<strong>IP:</strong> '+ server_data.ip + '<br /> <strong>Hostname: </strong> ' + server_data.hostname + '</p></div>'
 				$('#divServerList').append
 			}
 		});
