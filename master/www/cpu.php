9d18d8653ec5249e0e224483c35f3c6041319cd5
Justin Carmony <justin@justincarmony.com>
2012-05-22 22:59:49 -0500

    tweak to cpu.php

--- master/www/cpu.php
+++ master/www/cpu.php
@@ -15,21 +15,29 @@ error_reporting(E_ALL);
 $stats = new stdClass();
 
 $process_id = file_get_contents('/tmp/redis_process_id');
-$cmd = "pidstat -p $process_id 1 1";
-$output = array();
-exec($cmd, $output);
-
-$line = $output[3]; // get the line we're interested in
-str_replace("\t", ' ', $line); // Remove Tabs
-// Strip out extra spaces
-while(str_replace("  ", " ", $line) != $line)
+if(count($process_id) > 0)
 {
-	$line = str_replace("  ", " ", $line);
+    $cmd = "pidstat -p $process_id 1 1";
+    $output = array();
+    exec($cmd, $output);
+
+    $line = $output[3]; // get the line we're interested in
+    str_replace("\t", ' ', $line); // Remove Tabs
+// Strip out extra spaces
+    while(str_replace("  ", " ", $line) != $line)
+    {
+        $line = str_replace("  ", " ", $line);
+    }
+
+    $parts = explode(' ', $line);
+    $cpu = $parts[2];
+
+    $stats->lblCpuUsage = $cpu;
+}
+else
+{
+    $stats->error = 'Process ID Missing';
 }
 
-$parts = explode(' ', $line);
-$cpu = $parts[2];
-
-$stats->lblCpuUsage = $cpu;
 echo json_encode($stats);
 exit();
